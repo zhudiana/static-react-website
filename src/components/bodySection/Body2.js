bf68835af41ea22663157c2c9b596ca4674b6a24
judiana <yoditfentaw90@gmail.com>
2022-10-26 23:56:56 -0800

    body2-data commited

---
+++ src/components/bodySection/Body2.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Body.css";
+
+const Body2 = (props) => {
+  return (
+    <>
+      <div className="body2-container"></div>
+    </>
+  );
+};
+
+export default Body2;
--- src/components/bodySection/BodyCollection.js
+++ src/components/bodySection/BodyCollection.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Body1 from "./Body1";
+import Body2 from "./Body2";
+import { dummyData } from "../../data/dummyData";
 
 const BodyCollection = () => {
+  const body2Elements = dummyData.map((item) => {
+    return <Body2 image={item.image} />;
+  });
   return (
     <div>
       <Body1 />
+      {body2Elements}
     </div>
   );
 };
